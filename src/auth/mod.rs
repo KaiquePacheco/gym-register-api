2572f344ee6403149d368d2469b49ea84387695a
KaiquePacheco <kaique12br@gmail.com>
2024-02-08 21:52:06 -0300

    Define sign_in route in auth/mod.rs

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,13 +1,38 @@
+pub mod token;
+mod custom_errs;
 mod dtos;
+
+use std::ops::Deref;
 
 use dtos::{SignIn, SignUp};
 use rocket::{
     form::{Form, Strict},
     post,
+    response::status,
+    State,
 };
 
+use super::config::Configs;
+
+use super::database::ConnectionPool;
+
 #[post("/signin", data = "<sign_in_form>")]
-pub fn sign_in(sign_in_form: Form<Strict<SignIn<'_>>>) {}
+pub async fn sign_in<'r>(
+    pool: &State<ConnectionPool>,
+    configs: &State<Configs>,
+
+    sign_in_form: Form<Strict<SignIn<'_>>>,
+) -> Result<status::Accepted<String>, status::BadRequest<&'r str>> {
+    let conn = pool.get().await.unwrap();
+
+    let sign_in_form = sign_in_form.deref().deref();
+    let sign_token_result = token::sign_token(sign_in_form, &configs.jwt_key, conn).await;
+
+    match sign_token_result {
+        Ok(token) => Ok(status::Accepted(token)),
+        Err(_) => Err(status::BadRequest("Wrong email or password")),
+    }
+}
 
 #[post("/signup", data = "<sign_up_form>")]
-pub fn sign_up(sign_up_form: Form<Strict<SignUp<'_>>>) {}
+pub async fn sign_up(sign_up_form: Form<Strict<SignUp<'_>>>) {}
